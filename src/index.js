fa922275775aa6d0b3f56bff343eec4168652935
adminssi <walakh@gmail.com>
2018-09-14 19:30:21 +0300

    Square classista komponentti jolla ei omaa statea, X ja O vaihtelee.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
- 
-    render() {
+function Square(props) {
       return (
-        <button className="square" 
-        onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
     }
-  }
   
   class Board extends React.Component {
      /* Tämän voi myös nykyään toteuttaa getInitialState() methodilla */
@@ -24,13 +19,17 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -43,7 +42,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
